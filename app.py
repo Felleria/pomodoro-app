de5cf7821c1f72291e92a2eb52a465cca893633b
Felleria <tellytabbie@gmail.com>
2025-02-06 23:35:36 +0300

    🔧 Update files
    Update Files

---
+++ app.py
@@ -0,0 +1,26 @@
+
+"""
+Welcome to Chonjo App.
+Chonjo is a simple Commandline Pomodoro App
+Usage:
+    app.py pomodoro_start <task-title>...
+    app.py pomodoro_config_time <duration-in-minutes>
+    app.py pomodoro_config_short_break <duration-in-minutes>
+    app.py pomodoro_config_long_break <duration-in-minutes>
+    app.py pomodoro_config_sound <off/on>
+    app.py pomodoro_stop
+    app.py pomodoro_list
+    app.py (-i | --interactive)
+    app.py (-h | --help)
+    app.py (-v | --version)
+    app.py quit
+    quit
+Arguments
+    <task-title>            Name of the task at hand
+    <duration-in-minutes>   Duration of in minutes
+    <off/on>                The states of the sound
+Options:
+    -i --interactive        Interactive Mode
+    -h --help               Show this screen and exit
+    -v --version
+"""
